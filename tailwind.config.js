792be3a58fdc7420b98475af815ce6332f44b692
diahnkha <diahnurkhasanah5@gmail.com>
2023-02-28 22:42:11 +0700

    add statuses

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
+    mode: 'jit',
+    purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -11,33 +11,36 @@ module.exports = {
 
     theme: {
         extend: {
-            colors:{
+            colors: {
                 transparent: 'transparent',
                 current: 'currentColor',
+
                 black: colors.black,
                 white: colors.white,
-                gray: colors.neutral,
-                'blue' : '#328af1',
-                'blue-hover' : '#2879bd',
+                gray: colors.trueGray,
+                'gray-background': '#f7f8fc',
+                'blue': '#328af1',
+                'blue-hover': '#2879bd',
                 'yellow' : '#ffc73c',
                 'red' : '#ec454f',
+                'red-100' : '#fee2e2',
                 'green' : '#1aab8b',
+                'green-50': '#f0fdf4',
                 'purple' : '#8b60ed',
-                'gray-background':'#f7f8fc',
             },
-            spacing:{
+            spacing: {
                 22: '5.5rem',
                 44: '11rem',
                 70: '17.5rem',
                 76: '19rem',
-                104: '26rem',  
-                175: '43.75rem'
-                
+                104: '26rem',
+                128: '32rem',
+                175: '43.75rem',
             },
-            maxWidth:{
-                custom: '68.5rem'
+            maxWidth: {
+                custom: '68.5rem',
             },
-            boxShadow:{
+            boxShadow: {
                 card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
                 dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
             },
@@ -45,13 +48,19 @@ module.exports = {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
             fontSize: {
-                xxs: ['0.625rem', {lineHeight: '1rem'} ],
-            }
+                xxs: ['0.625rem', { lineHeight: '1rem' }],
+            },
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
         },
     },
 
     plugins: [
         require('@tailwindcss/forms'),
-        require('@tailwindcss/line-clamp')
+        require('@tailwindcss/line-clamp'),
     ],
 };
