546ee163620607d49476c9f6bff098261045c902
diahnkha <diahnurkhasanah5@gmail.com>
2023-03-03 17:39:56 +0700

    add comments

--- app/Http/Livewire/IdeaComments.php
+++ app/Http/Livewire/IdeaComments.php
@@ -9,6 +9,13 @@ class IdeaComments extends Component
 {
     public $idea;
 
+    protected $listeners = ['commentWasAdded'];
+
+    public function commentWasAdded()
+    {
+        $this->idea->refresh();
+    }
+
     public function mount(Idea $idea)
     {
         $this->idea = $idea;
@@ -18,7 +25,6 @@ class IdeaComments extends Component
     {
         return view('livewire.idea-comments', [
             'comments' => $this->idea->comments,
-            
         ]);
     }
 }
