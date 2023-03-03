8de6ce0e6afc6b9b2595ebe474eedbb0adc17c2a
diahnkha <diahnurkhasanah5@gmail.com>
2023-03-03 14:31:58 +0700

    add Success Message Notification works

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,6 +104,13 @@
                 </div>
             </div>
         </main>
+
+        @if (session('success_message'))
+            <x-notification-success
+                :redirect="true"
+                message-to-display="{{ (session('success_message')) }}"
+            />
+        @endif
         <livewire:scripts />
     </body>
 </html>
